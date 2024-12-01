e749d6e3d72427cace9d6e7a013ca72727291fb5
RicardoMartines85 <ricardo.martines@live.com>
2024-12-01 12:15:08 -0300

    ProjetoFinalizado

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log ("inicio desafio")
+// Desafio Felipão desvendando o mundo de Zelda!
+
+// declaração de variaveis
+let heroi = "Link"
+let qtdXP = 7458
+let nivel = ""
+
+//metodo
+
+switch(true){
+    case (qtdXP<1001):
+   nivel = "ferro"   
+    break
+
+    case (qtdXP<2001):
+   nivel = "Bronze"   
+    break
+
+    case (qtdXP<5001):
+   nivel = "Prata"   
+    break
+
+    case (qtdXP<7001):
+   nivel = "Ouro"   
+    break
+
+    case (qtdXP<8001):
+   nivel = "Plantina"   
+    break
+
+    case (qtdXP<9001):
+   nivel = "Ascendente"   
+    break
+
+    case (qtdXP<10001):
+   nivel = "Imortal"   
+    break
+
+    default:
+        nivel = "Radiante"
+
+}
+//saída
+
+console.log ("O Herói de nome "+heroi+" está no nível de "+nivel)
